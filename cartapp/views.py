f1198272adf60e146243debccfbaaaad78d3cd0d
Shankar Nag <shankaarnag36@gmail.com>
2021-05-27 11:00:59 +0530

    added cart app

--- cartapp/views.py
+++ cartapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts iter render,redirect,get_object_or_404
+from django.views.decorators.http import require_GET, require_POST
+from myshop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+
+from django.utils.translation import override
 
 # Create your views here.
+
+@require_POST
+def cart_add(request,product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product,id = product_id)
+    form = CartAddProductForm(require.POST)
+    if form.is_valid:
+        cd = form.cleaned_data
+        cart.add(product = product,quantity = cd['quantity'],override_quantity = cd['override'])
+    
+    
+    return redirect('cart:cart_detail')
+
+@require_POST
+def cart_remove(request,prodct_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product,id = prodct_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+    
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request,'cart/detail.html',{'cart':cart})
+
+
+
+
+
+
--- myshopppingsite/urls.py
+++ myshopppingsite/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/',include('cartapp.urls', namespace = 'cart')),
     path('',include('myshop.urls',namespace = 'myshop')),
 ]
